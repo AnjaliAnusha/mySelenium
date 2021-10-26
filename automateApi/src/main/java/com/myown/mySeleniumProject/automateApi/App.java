bc71ae327b8e8fecd7da9c6258504ab44804c9d1
AnjaliAnusha <anjalikanth24@gmail.com>
2021-10-26 14:30:46 -0400

    initial commit

---
+++ automateApi/src/main/java/com/myown/mySeleniumProject/automateApi/App.java
@@ -0,0 +1,13 @@
+package com.myown.mySeleniumProject.automateApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
